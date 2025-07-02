dd3275b7efebe1b16aa16186664ab2f74571a8e6
FARTASH-421 <abdul.qhadir.fartash@gmail.com>
2025-07-02 17:18:01 +0330

    navbar completed

--- app/page.tsx
+++ app/page.tsx
@@ -3,18 +3,29 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <nav className="bg-blue-950 left-0 right-0 h-20 flex flex-row items-center justify-around">
-        <Link href={"/auth"}>
-          <div className="bg-amber-200 py-2 px-6 font-bold text-2xl rounded-xl hover:bg-amber-400 ">
-            Login
+      <nav className="bg-gray-100 left-0 right-0 h-20 flex flex-row items-center justify-between px-20 shadow-xl shadow-orange-100">
+        <Link href={"/"}>
+          <div className=" bg-amber-100 py-1 px-6 font-bold text-xl rounded-xl hover:bg-amber-200 ">
+            Home
           </div>
         </Link>
-        <Link href={"/dashboard"}>
-          <div className="bg-amber-200 py-2 px-6 font-bold text-2xl rounded-xl hover:bg-amber-400 ">
-            Dashboard
-          </div>
-        </Link>
+        <div className="flex gap-x-6">
+          <Link href={"/auth"}>
+            <div className="bg-amber-100 py-1 px-6 font-bold text-xl rounded-xl hover:bg-amber-200 ">
+              Login
+            </div>
+          </Link>
+          <Link href={"/dashboard"}>
+            <div className="bg-amber-100 py-1 px-6 font-bold text-xl rounded-xl hover:bg-amber-200 ">
+              Dashboard
+            </div>
+          </Link>
+        </div>
       </nav>
+
+      <div className="flex flex-col items-center justify-center mt-20">
+        <h1 className="font-bold text-2xl">Home Page </h1>
+      </div>
     </div>
   );
 }
